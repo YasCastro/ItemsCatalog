828554333adddae9a27b35e702326ab08c68a287
Yasmine C. Fortes <yazzcastrof@gmail.com>
2023-07-05 16:11:00 -0300

    First commit

---
+++ Repositories/IItemsRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ItemsCatalog.Entities;
+
+namespace ItemsCatalog.Repositories
+{
+  public interface IItemsRepository
+  {
+    Task<Item> GetItemAsync(Guid id);
+    Task<IEnumerable<Item>> GetItemsAsync();
+    Task CreateItemAsync(Item item);
+    Task UpdateItemAsync(Item item);
+    Task DeleteItemAsync (Guid id);
+
+  }
+}
